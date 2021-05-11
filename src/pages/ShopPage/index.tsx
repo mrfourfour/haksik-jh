2e2886966886100f5fbe4b216c4b24e51aff4c0b
aerain <j40f893@gmail.com>
2021-05-11 21:50:39 +0900

    feat: add navigation

---
+++ src/pages/ShopPage/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+function ShopPage() {
+    return (
+        <View>
+            <Text>Shop Page</Text>
+        </View>
+    )
+}
+
+export default ShopPage;
